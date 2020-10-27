32e509a506b41e82098aa82fd665fdbf5563cced
rohan@simpragma.com <roshnibly95#gmail.com>
2020-10-27 10:35:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Dragula from 'react-dragula';
+import { Input } from 'antd';
+import './App.scss';
 
-function App() {
+const App = (props) => {
+  const [disabled, setDisabled] = useState(true);
+
+  const dragulaDecorator = (componentBackingInstance) => {
+    if (componentBackingInstance) {
+      let options = {};
+      console.log(componentBackingInstance);
+      Dragula([componentBackingInstance], options);
+    }
+  };
+  const onChange = (e) => {
+    console.log(e);
+  };
+
+  const handlehandleFocus = () => {
+    setDisabled(false);
+  };
+  console.log(disabled);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='container' ref={dragulaDecorator}>
+      <div className='sub-container'>asdasd</div>
+      <div className='sub-container_1'>
+        <div className='d_none'>
+          <button>Edit</button>
+          <button>Setting</button>
+          <button>Move</button>
+        </div>
+        <div className='textArea_container' onClick={handlehandleFocus}>
+          <form>
+            <Input
+              onChange={onChange}
+              onBlur={() => setDisabled(true)}
+              defaultValue={
+                'Swap them aroundSwap them aroundSwap them aroundSwap them aroundSwaphem aroundSwap them aroundSwap them aroundSwap them around'
+              }
+              disabled={disabled}
+            />
+          </form>
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Swap them aroundSwap them aroundSwap them aroundSwap them aroundSwap
+          them aroundSwap
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Swap them aroundSwap them aroundSwap them aroundSwap them aroundSwap
+          them aroundSwap
+        </p>
+      </div>
+      <div className='sub-container'>Swap them around</div>
+      <div className='sub-container'>Swap us around</div>
+      <div className='sub-container'>Swap things around</div>
+      <div className='sub-container'>Swap everything around</div>
     </div>
   );
-}
+};
 
 export default App;
